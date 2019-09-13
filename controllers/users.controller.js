9cb2a5b36ded64b9145db784f2f995f8459ba234
ChiNguyen <dacchi6761@gmail.com>
2019-09-13 23:00:01 +0700

    Complete register feature

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,4 +1,4 @@
-//const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/user.model');
 
@@ -6,22 +6,31 @@ module.exports.renderRegisterPage = function (req, res) {
 	res.render('users/register', { title: 'Register' });
 };
 
-module.exports.registerUser = function (req, res) {
-	const username = req.body.name;
-	const email = req.body.email;
-	const password = req.body.password;
-	const confirmPassword = req.body.confirmPassword;
-
-	// const hashPassword = createHashPassword(password);
-	const newUser = new User({ username: username, email: email, password: password });
-	newUser.save().then((user) => console.log(user));
+module.exports.renderLoginPage = function (req, res) {
+	res.render('users/login', { title: 'Login' });
 }
 
-// function createHashPassword(password) {
-// 	bcrypt.genSalt(10, (err, salt) => {
-// 		bcrypt.hash(password, salt, (err, hashPassword) => {
-// 			if (err) throw err;
-// 			return hashPassword;
-// 		})
-// 	})
-// }
+module.exports.registerUser = function (req, res) {
+	const username = req.body.username;
+	const email = req.body.email;
+	const password = req.body.password;
+
+	User.findOne({ email: email }, (err, user) => {
+		if (err) throw err;
+		if (user) {
+			req.flash('msg-valid-err', 'Email already exists!');
+			res.location('/users/register');
+			res.redirect('/users/register');
+		} else {
+			bcrypt.genSalt(10, (err, salt) => {
+				bcrypt.hash(password, salt, (err, hashPassword) => {
+					const newUser = new User({ username: username, email: email, password: hashPassword });
+					newUser.save().then((user) => console.log(user));
+					req.flash('msg-success', 'Register successfully!');
+					res.location('/users/login');
+					res.redirect('/users/login');
+				})
+			})
+		}
+	})
+};
--- routes/users.route.js
+++ routes/users.route.js
@@ -6,6 +6,9 @@ const validateMiddleware = require('../middleware/validate.middleware');
 
 route.get('/register', usersController.renderRegisterPage);
 
+route.get('/login', usersController.renderLoginPage);
+
 route.post('/register', validateMiddleware.validateRegisterForm, usersController.registerUser);
+
 
 module.exports = route;
